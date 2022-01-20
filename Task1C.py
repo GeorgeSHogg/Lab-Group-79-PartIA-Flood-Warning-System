b2957a0c8127169ebd3d06b301ab2d1158fd92c9
GeorgeSHogg <georgeshogg@outlook.com>
2022-01-20 12:17:52 +0000

    Inital Commit

---
+++ Task1C.py
@@ -0,0 +1,9 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import stations_within_radius
+
+r = 10
+p = (52.2053, 0.1218)
+stations = build_station_list()
+
+stations = stations_within_radius(stations, p, r)
+print(sorted([station.name for station in stations]))
