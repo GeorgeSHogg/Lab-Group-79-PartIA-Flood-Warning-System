a9540abc52e5f165e2550d33ec9ee88b9b9f1542
GeorgeSHogg <georgeshogg@outlook.com>
2022-01-20 12:02:44 +0000

    Ammended for Task 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -35,3 +35,8 @@ def stations_by_distance(stations, p):
     """Finds the distance of all stations to a specified point and returns them as an ordered list sorted by ascending distance"""
     stations = [(station, distance(station.coord, p)) for station in stations]
     return sorted(stations, key = lambda station: station[1])
+
+def stations_within_radius(stations, centre, r):
+    stations_distance = [distance(station.coord, centre) for station in stations]
+    within_r = np.where(stations_by_distance <= r)
+    return(stations[within_r])
