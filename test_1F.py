853300e7dd4a6ed1ebd5ffa7904339c693cfb874
GeorgeSHogg <georgeshogg@outlook.com>
2022-01-23 15:22:29 +0000

    Ammendments to tests for 1F

--- test_1F.py
+++ test_1F.py
@@ -4,14 +4,16 @@ def test_typical_range_consistent():
     stations = build_station_list()
     
     station1 = stations[0]
-    station1.typical_range = False
+    station1.typical_range = None
 
-    station2 = stations[0]
+    station2 = stations[1]
     station2.typical_range = (100, 1)
 
-    station3 = stations[0]
+    station3 = stations[2]
     station3.typical_range = (1, 100)
 
-    assert station1.typical_range_consistent == False
-    assert station2.typical_range_consistent == False
-    assert station3.typical_range_consistent == True
+    assert station1.typical_range_consistent() == False
+    assert station2.typical_range_consistent() == False
+    assert station3.typical_range_consistent() == True
+
+test_typical_range_consistent()
