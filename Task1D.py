b167ea5f2ffcd7e3c89427934f5b57da708ddc23
Chris Hew <ch938@cam.ac.uk>
2022-02-10 12:32:52 +0000

    Added run()

--- Task1D.py
+++ Task1D.py
@@ -1,22 +1,24 @@
 from floodsystem.geo import rivers_with_station, stations_by_river
 from floodsystem.stationdata import build_station_list
 """Requirements for Task 1D"""
-stations = build_station_list()
-rivers = rivers_with_station(stations)
-s = stations_by_river
+def run():
 
-#First 10 rivers sorted
-rivers = sorted(rivers)
-r = rivers[:10]
+    stations = build_station_list()
+    rivers = rivers_with_station(stations)
+    s = stations_by_river
 
-#Print No. of stations with at least one monitoring station
-print(len(rivers))
-#First 10 - , rivers_with_station(stations))
-print(r)
+    #First 10 rivers sorted
+    rivers = sorted(rivers)
+    r = rivers[:10]
 
-#Stations on rivers Aire, Cam & Thames
-print("Stations on River Aire:", s(stations, "River Aire"))
-print("Stations on River Cam:", s(stations, "River Cam"))
-print("Stations on River Thames:", s(stations, "River Thames"))
+    #Print No. of stations with at least one monitoring station
+    print(len(rivers))
+    #First 10 - , rivers_with_station(stations))
+    print(r)
 
+    #Stations on rivers Aire, Cam & Thames
+    print("Stations on River Aire:", s(stations, "River Aire"))
+    print("Stations on River Cam:", s(stations, "River Cam"))
+    print("Stations on River Thames:", s(stations, "River Thames"))
+run()
 
