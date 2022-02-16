50f6d04412c1f85a462563b0b08fb99328adda9c
Chris Hew <ch938@cam.ac.uk>
2022-02-16 23:29:55 +0000

    Merge branch 'main' of https://github.com/GeorgeSHogg/Lab-Group-79-PartIA-Flood-Warning-System

---
+++ floodsystem/analysis.py
@@ -0,0 +1,13 @@
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime 
+from floodsystem.stationdata import update_water_levels
+
+def polyfit(dates, levels, p):
+ 
+    dates = []
+ 
+    levels = []
+ 
+    p = []
---
+++ floodsystem/plot.py
@@ -0,0 +1,24 @@
+import matplotlib as plt
+from datetime import datetime, timedelta
+from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import update_water_levels
+
+def plot_water_levels(station, dates, levels):
+
+    station = build_station_list()
+
+    dates = datetime()
+
+    levels = update_water_levels()
+
+#Plot with axes
+    plt.plot(dates, levels)
+
+#Added titles and labelled axes onto plots
+    plt.xlabel("Time for a station (s)")
+    plt.ylabel("Water Level (m)")
+    plt.title(station)
+
+#Display Plot
+    plt.tight_layout()
+    plt.show()
