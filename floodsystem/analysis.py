6cb1662bb8533896a09301f0a4954f38b245a846
Chris Hew <ch938@cam.ac.uk>
2022-02-26 23:48:23 +0000

    Task 2E/F & Test 2E

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -3,14 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime 
 from floodsystem.stationdata import update_water_levels
+from matplotlib.dates import date2num 
 
+#Finds least-squares fit of a polynomial of degree p to water level data
 def polyfit(dates, levels, p):
- 
-    dates = []
- 
-    levels = []
- 
-    p = []
 
+    x = matplotlib.dates.date2num(dates)
+    y = levels
 
-    return polyfit()
+    p_coeff = np.polyfit(x-x[0],y,p) #shift x values
+    poly = np.poly1d(p_coeff)
+    d0 = matplotlib.dates.date2num(dates[0])
+
+    return poly, d0
