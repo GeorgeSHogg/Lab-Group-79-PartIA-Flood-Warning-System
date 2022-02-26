eb593b2410c6affe6abba9b41e322e4586508e1d
Chris Hew <ch938@cam.ac.uk>
2022-02-26 22:25:02 +0000

    Initial Commit (Tried to make life easy with station.py)

---
+++ Task2E.py
@@ -0,0 +1,29 @@
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.plot import plot_water_levels
+from floodsystem.flood import most_at_risk_stations
+import matplotlib.pyplot as plt
+import datetime
+
+def run():
+    """Requirements for Task 2E"""
+
+    stations = build_station_list()
+    update_water_levels(stations)
+    stations_used = []
+    highest_level_stations = most_at_risk_stations(stations, 10)
+
+    for station in highest_level_stations:
+        stations_used.append(station[0])
+
+    dates = []
+    levels = []
+    
+    for station in stations_used:
+        dates, levels = fetch_measure_levels(station.measure_id, datetime.timedelta(days=10))
+        plot_water_levels(station,dates,levels)
+        plt.show()
+
+if __name__ == "__main__":
+    print("***Task 2E: CUED Part 1A Flood Warning System")
+    run()
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,23 +1,27 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from floodsystem.stationdata import build_station_list
-from floodsystem.stationdata import update_water_levels
+import numpy as np
 
 def plot_water_levels(station, dates, levels):
 
     station = build_station_list()
-
-    dates = datetime()
-
-    levels = update_water_levels()
+    dates = []
+    levels = []
+    low = station.get_typical_range()[0]
+    high = station.get_typical_range()[1]
 
 #Plot with axes
     plt.plot(dates, levels)
+    plt.plot(dates, low)
+    plt.plot(dates, high)
 
 #Added titles and labelled axes onto plots
-    plt.xlabel("Time for a station (s)")
+    plt.xlabel("Time for a station (days)")
     plt.ylabel("Water Level (m)")
     plt.title(station)
+    plt.xticks(rotation = 90)
+    plt.title("Station: " + station.get_stationName())
 
 #Display plot
     plt.tight_layout()
