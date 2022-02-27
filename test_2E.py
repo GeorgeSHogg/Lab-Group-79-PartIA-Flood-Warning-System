abadc857a2b5309daca81df6d7882057b64a2448
GeorgeSHogg <98087545+GeorgeSHogg@users.noreply.github.com>
2022-02-27 17:53:29 +0000

    Revert "Task 2E/F & Test 2E"
    This reverts commit 6cb1662bb8533896a09301f0a4954f38b245a846.

--- test_2E.py
+++ test_2E.py
@@ -3,12 +3,18 @@ import matplotlib.pyplot as plt
 
 def test_graphs(): 
     name = "Test station"
+<<<<<<< HEAD
     dates=["2012","2013","2014"]
     levels=[1.01,1.04,1.02]
+=======
+    dates = []
+    levels = []
+>>>>>>> parent of d0fae15 (Commented)
     
     plt.plot(dates, levels)
 
 # Add axis labels, rotate date labels and add plot title
+<<<<<<< HEAD
     plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation = 45);
@@ -16,4 +22,13 @@ def test_graphs():
     plt.show()
     plt.tight_layout()
     
+=======
+    plt.xlabel('Date (days)')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation = 45)
+    plt.title(name)
+    plt.show()
+    plt.tight_layout()
+
+>>>>>>> parent of d0fae15 (Commented)
     assert plt.plot.called
