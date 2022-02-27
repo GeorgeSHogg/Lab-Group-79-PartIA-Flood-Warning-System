e945d21c479b88bbf9a7f81f69e454cdbf5a5f81
GeorgeSHogg <98087545+GeorgeSHogg@users.noreply.github.com>
2022-02-27 17:49:31 +0000

    Revert "Commit"
    This reverts commit bcdb81fdec22b356a43459e64fcc402432c18b71.

--- test_2E.py
+++ test_2E.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 def test_graphs(): 
     name = "Test station"
-    dates = []
-    levels = []
+    dates=["2012","2013","2014"]
+    levels=[1.01,1.04,1.02]
     
     plt.plot(dates, levels)
 
 # Add axis labels, rotate date labels and add plot title
-    plt.xlabel('Date (days)')
+    plt.xlabel('date')
     plt.ylabel('water level (m)')
     plt.xticks(rotation = 45)
     plt.title(name)
