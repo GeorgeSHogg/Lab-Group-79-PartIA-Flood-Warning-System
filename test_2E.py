bcdb81fdec22b356a43459e64fcc402432c18b71
Chris Hew <ch938@cam.ac.uk>
2022-02-27 10:57:19 +0000

    Commit

--- test_2E.py
+++ test_2E.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 def test_graphs(): 
     name = "Test station"
-    dates=["2012","2013","2014"]
-    levels=[1.01,1.04,1.02]
+    dates = []
+    levels = []
     
     plt.plot(dates, levels)
 
 # Add axis labels, rotate date labels and add plot title
-    plt.xlabel('date')
+    plt.xlabel('Date (days)')
     plt.ylabel('water level (m)')
     plt.xticks(rotation = 45)
     plt.title(name)
