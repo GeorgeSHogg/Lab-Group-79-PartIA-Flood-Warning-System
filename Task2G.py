a42cae0b427b7ac86b4f17d4965f1d7ad4f33b1b
GeorgeSHogg <98087545+GeorgeSHogg@users.noreply.github.com>
2022-02-28 21:28:53 +0000

    added range

--- Task2G.py
+++ Task2G.py
@@ -3,6 +3,7 @@ from floodsystem.flood import stations_level_over_threshold
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.analysis import polyfit, predict_max
+from floodsystem.geo import stations_within_radius
 import matplotlib
 import numpy as np
 
@@ -12,6 +13,11 @@ def breakdown(station):
 
 def run():
     "Requirements for Task 2B"
+    severe_towns = []
+    high_towns = []
+    moderate_towns = []
+    low_towns = []
+
     stations = build_station_list()
     update_water_levels(stations)
     tol = 1
@@ -21,26 +27,38 @@ def run():
     order = 3
     future_days = 5
     min_data_threshold = 5
-    min_severe = 1.5
-
+    min_severe = 1.75
+    min_high = 1.25
+    
     at_severe_risk = []
+    at_high_risk = []
     for station in at_risk:
         dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days = 2))
         if len(dates) > min_data_threshold:    
             predicted = predict_max(station, dates, levels, order, future_days)
             if predicted > min_severe:
                 at_severe_risk.append(station)
+            elif predicted > min_high:
+                at_high_risk.append(station)
 
     print("Stations at severe risk")
     print(*[station.name for station in at_severe_risk], sep = ", ")
     
     at_risk_rivers = []
+    shortlist = []
+    radius = 20
+
     for station in at_severe_risk:
-        if station.river not in at_risk_rivers:
-            at_risk_rivers.append(station.river)
+        if station.town not in severe_towns:
+            severe_towns.append(station.town)
+        for nearby_station in stations_within_radius(stations, station.coord, radius):
+            shortlist.append(nearby_station)
+        
+  
+    print(*shortlist, sep = ", ")
+
     
-    print("Rivers with severe risk stations")
-    print(*at_risk_rivers, sep = ", ")
+
 
 
 
