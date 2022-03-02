88fb7c6de756ae745a2d3eb4e83811a57e24ac42
GeorgeSHogg <98087545+GeorgeSHogg@users.noreply.github.com>
2022-03-02 12:26:33 +0000

    added check for data

--- Task2E.py
+++ Task2E.py
@@ -12,25 +12,34 @@ def run():
     stations = build_station_list()
     update_water_levels(stations)
     stations_used = []
-    highest_level_stations = most_at_risk_stations(stations, 5)
+    highest_level_stations = most_at_risk_stations(stations, 10)
 
     station_list = []
     dates_list = []
     levels_list = []
 
+    n = 5
+
     for station in highest_level_stations:
-        stations_used.append(station[0])
+        dates, levels = fetch_measure_levels(station[0].measure_id, datetime.timedelta(days=10))
+        if len(dates) > 3:
+            stations_used.append(station[0])
+            n -= 1
+        else:
+            print("Station ", station[0].name, "does not have sufficient recent data to plot")
+        if not n:
+            break
 
     for station in stations_used:
         dates, levels = fetch_measure_levels(station.measure_id, datetime.timedelta(days=10))
-        if len(dates) > 3:
-            dates = np.array(dates)
-            levels = np.array(levels)
-            plot_water_levels([station], [dates] , [levels])
+        dates = np.array(dates)
+        levels = np.array(levels)
+        plot_water_levels([station], [dates] , [levels])
 
-            station_list.append(station)
-            dates_list.append(dates)
-            levels_list.append(levels)
+        station_list.append(station)
+        dates_list.append(dates)
+        levels_list.append(levels)
+        
 
     plot_water_levels(station_list, dates_list, levels_list, False)
 
--- Task2F.py
+++ Task2F.py
@@ -7,14 +7,24 @@ from floodsystem.stationdata import build_station_list, update_water_levels
 def run():
     stations= build_station_list()
     update_water_levels(stations)
-    high_risk_stations = most_at_risk_stations(stations, 5)
-  
-    for station, _ in high_risk_stations:
+    highest_level_stations = most_at_risk_stations(stations, 10)
+    
+    stations_used = []
+    n = 5
+ 
+    for station in highest_level_stations:
+        dates, levels = fetch_measure_levels(station[0].measure_id, dt = timedelta(days=2))
+        if len(dates) > 3:
+            stations_used.append(station[0])
+            n -= 1
+        else:
+            print("Station ", station[0].name, "does not have sufficient recent data to plot")
+        if not n:
+            break
+
+    for station in stations_used:
         dates, levels = fetch_measure_levels(station.measure_id, dt=timedelta(days = 2))
-        try:
-            graph = plot_water_level_with_fit(station, dates, levels, 4)
-        except:
-            print('No data')
+        plot_water_level_with_fit(station, dates, levels, 4)
         
 if  __name__ == "__main__":
     print("*** Task 2F: CUED Part IA Flood Warning System ***")
